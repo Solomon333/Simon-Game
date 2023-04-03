2044241d383ebc3a3805bbaec19d1da5116e6417
Salomon Solomon <ssalomon783@gmail.com>
2023-04-03 15:11:05 +0300

    add jquery

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-let buttonColors = ["red", "blue", "green", "yellow"];
+let buttonColors= ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-let randomChosenNumber = buttonColors[nextSequence()];
+let userClickedPattern = [];
 
-gamePattern.push(randomChosenNumber);
+$(".btn").click(function(){
+    
+    let userChosenColor = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    
+});
 
 function nextSequence() {
-    let randomNumber = Math.floor(Math.random()*4);   
-    return randomNumber;
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+    animatePress(randomChosenColor);
+    
 }
 
-$("#green").on("click", function(){
 
+function playSound(name) {
 
-})
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+} 
+
+function animatePress(currentColor) {
+   
+     $("#" + currentColor).addClass("pressed");
+    setTimeout(function(){
+     $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+$("body").one("keydown", function () {
+    let level1 = 0;
+    $("#level-title").text("Level 0")
+    nextSequence();
+});
